e335de9c372aa53417ffdc0cf93346b073a2a8b7
tommytnez <tommytnez@yahoo.com>
2017-04-17 16:43:15 -0700

    yes

--- bin/drop.js
+++ bin/drop.js
@@ -1,19 +1,19 @@
-// 'use strict';
-// require('dotenv').config({path: `${__dirname}/../.env`});
-//
-// const mongoose = require('mongoose');
-// mongoose.Promise = global.Promise;
-// mongoose.connect(process.env.MONGODB_URI);
-//
-// const User = require('../model/user.js');
-// const Appointment= require('../model/appointment.js');
-//
-// Promise.all([
-//   User.remove({}),
-//   Appointment.remove({}),
-// ])
-// .then(()=> mongoose.disconnect())
-// .catch(err => {
-//   console.error(err);
-//   mongoose.disconnect();
-// });
+'use strict';
+require('dotenv').config({path: `${__dirname}/../.env`});
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI);
+
+const User = require('../model/user.js');
+const Appointment= require('../model/appointment.js');
+
+Promise.all([
+  User.remove({}),
+  Appointment.remove({}),
+])
+.then(()=> mongoose.disconnect())
+.catch(err => {
+  console.error(err);
+  mongoose.disconnect();
+});
--- bin/populate.js
+++ bin/populate.js
@@ -1,21 +1,21 @@
-// 'use strict';
-// require('dotenv').config({path: `${__dirname}/../.env`});
-//
-// const mongoose = require('mongoose');
-// mongoose.Promise = global.Promise;
-// mongoose.connect(process.env.MONGODB_URI);
-//
-// const userMocks = require('../test/lib/user-mocks');
-// const appointmentMocks = require ('../test/lib/appointment-mocks');
-//
-// function populateStuff(){
-//   userMocks.call(this, (err) => {
-//     if (err) return console.log(err);
-//     appointmentMocks.call(this, (err)=>{
-//       if (err) return console.log(err);
-//       mongoose.disconnect();
-//     });
-//   });
-// }
-//
-// populateStuff.call({});
+'use strict';
+require('dotenv').config({path: `${__dirname}/../.env`});
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI);
+
+const userMocks = require('../test/lib/user-mocks');
+const appointmentMocks = require ('../test/lib/appointment-mocks');
+
+function populateStuff(){
+  userMocks.call(this, (err) => {
+    if (err) return console.log(err);
+    appointmentMocks.call(this, (err)=>{
+      if (err) return console.log(err);
+      mongoose.disconnect();
+    });
+  });
+}
+
+populateStuff.call({});
